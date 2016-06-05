b081d8daf57ddbecba462d7a8b03966918ae4663
yonggoonoh <nohyg90@gmail.com>
2016-06-05 13:43:08 +0000

    AF

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-</head>
-<body>
 <?php
-include "db_info.php";
-$result=mysql_query("select * from mysql.temp");
+$tpl = "index.tpl.php";
+include "template.php";
 ?>
-<?php
-while($row = mysql_fetch_array($result)) {
-	echo "<h1>".$row['name']."</h1>";
-}
-?>
-</body>
-</html>
