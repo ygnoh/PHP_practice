bad39e663b444e02c3b9c6955c3cabdd9f578267
yonggoonoh <nohyg90@gmail.com>
2016-06-05 14:04:29 +0000

    Basic login form at index.tpl.php

--- index.tpl.php
+++ index.tpl.php
@@ -1,6 +1,22 @@
-<?php
-	$result=mysql_query("select * from member");
-	while($row = mysql_fetch_array($result)) {
-		echo "<h1>".$row['mem_email']."</h1>";
-	}
-?>
+<form action="./sign_in.php?mode=insert" method="POST">
+	<table>
+		<tr>
+			<th></th>
+			<th>고객</th>
+			<th>직원</th>
+		</tr>
+		<tr>
+			<td>email</td>
+			<td colspan='2'><input type="email" placeholder="email" /></td>
+		</tr>
+		<tr>
+			<td>password</td>
+			<td colspan='2'><input type="password" placeholder="password" /></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td>회원가입</td>
+			<td>로그인</td>
+		</tr>
+	</table>
+</form>
